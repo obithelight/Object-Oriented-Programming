df0c3ce9d558488f01d3b7eaa03fcdb966146691
Kenneth Onu <kennetho.onu@gmail.com>
2022-06-03 06:16:32 +0800

    Create 4 Student Objects Computing their Average Grades

--- student-grades.js
+++ student-grades.js
@@ -128,6 +128,12 @@ let studentThree = {
 		console.log(`${this.name}'s quarterly averages are: ${this.grades}`)
 	},
 
+	computeAve(){
+		let sum = 0
+		this.grades.forEach(grade => sum = sum + grade)
+		return sum/4
+	},
+
 	
 
 
