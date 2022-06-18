9f29cb6cf85e5cccfb4b64de1dbc855fc0840119
Kenneth Onu <kennetho.onu@gmail.com>
2022-06-19 05:45:31 +0800

    Create 4 Student Objects Computing their Average Grades

---
+++ student-grades.js
@@ -0,0 +1,17 @@
+//FUNCTION CODING:
+
+//Translate the other students specifics into their own respective objects. Make sure to include the login, logout, and listGrades method in each object, as well.
+
+/*
+Name: Joe
+Email: joe@mail.com
+Grades: 78, 82, 79, 85
+
+Name: Jane
+Email: jane@mail.com
+Grades: 87, 89, 91, 93
+
+Name: Jessie
+Email: jessie@mail.com
+Grades: 91, 89, 92, 93
+*/
