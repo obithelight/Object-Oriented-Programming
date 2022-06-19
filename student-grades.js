ad96a30ff648405dd62dddc766aac44ae9b00409
Kenneth Onu <kennetho.onu@gmail.com>
2022-06-20 05:51:39 +0800

    Create 4 Student Objects Computing their Average Grades

--- student-grades.js
+++ student-grades.js
@@ -21,3 +21,5 @@ Grades: 91, 89, 92, 93
 //Define a method for all student objects named willPass() that returns a Boolean value indicating if student will pass or fail. For a student to pass, their ave. grade must be greater than or equal to 85.
 
 //Define a method for all student objects named willPassWithHonors() that returns true if ave. grade is greater than or equal to 90, false if >= 85 but < 90, and undefined if < 85 (since student will not pass).
+
+//Create an object named classOf1A with a property named students which is an array containing all 4 student objects in it.
