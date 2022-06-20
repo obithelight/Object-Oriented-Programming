f0eb123e3c8f45746f7f49fea5922b5b01f5bbd8
Kenneth Onu <kennetho.onu@gmail.com>
2022-06-21 05:54:40 +0800

    Create 4 Student Objects Computing their Average Grades

--- student-grades.js
+++ student-grades.js
@@ -27,3 +27,5 @@ Grades: 91, 89, 92, 93
 //Create a method for the object classOf1A named countHonorStudents() that will return the number of honor students.
 
 //Create a method for the object classOf1A named honorsPercentage() that will return the % of honor students from the batch's total number of students.
+
+//Create a method for the object classOf1A named retrieveHonorStudentInfo() that will return all honor students' emails and ave. grades as an array of objects.
