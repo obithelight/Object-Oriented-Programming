31a89d4d2bff202b843d496e83d6066efc16d71d
Kenneth Onu <kennetho.onu@gmail.com>
2022-06-30 19:44:33 +0800

    Add string to the end of an array

--- addtoend.js
+++ addtoend.js
@@ -1,3 +1,5 @@
 /*
 // Create a function named addToEnd that will add a passed in string to the end of a passed in array. If element to be added is not a string, return the string "error - can only add strings to an array". Otherwise, return the updated array. Use the students array and the string "Ryan" as arguments when testing.
 */
+
+let students = ["John", "Joe", "Jane", "Jessie"] 
