bed58b81b0223541770837fa9ec6c68008c32d11
Kenneth Onu <kennetho.onu@gmail.com>
2022-07-01 20:32:13 +0800

    Add string to the end of an array

--- addtoend.js
+++ addtoend.js
@@ -5,3 +5,14 @@ Create a function named addToEnd that will add a passed in string to the end of 
 //SWITCH STATEMENT
 let students = ["John", "Joe", "Jane", "Jessie"];
 
+const addToEnd = (string, array) => {
+  switch(typeof string !== 'string'){
+    case 1:
+      return "error - can only add strings to an array"
+      break;
+    default:
+      array.push(string)
+      return array
+      break;
+  }
+}
