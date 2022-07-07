b977759308fb9d7ca5da9201c5f4f40cb90fb0b9
Kenneth Onu <kennetho.onu@gmail.com>
2022-07-08 04:53:59 +0800

    Add string to the start of array

--- addtostart.js
+++ addtostart.js
@@ -2,19 +2,6 @@
 Create a function named addToStart that will add a passed in string to the start of a passed in array. If element to be added is not a string, return the string "error - can only add strings to an array". Otherwise, return the updated array. Use the students array and the string "Tess" as arguments when testing.
 */
 
-//IF STATEMENT
-let students = ["Ada", "Amaka", "Amanda", "Amara"];
-
-const addToStart = (string, array) => {
-  if (typeof string !== "string") {
-    return "error - can only add strings to an array";
-  } else {
-    array.unshift(string);
-    return array;
-  }
-};
-console.log(addToStart("Tess", students));
-
 //SWITCH STATEMENT
 let students = ["Ada", "Amaka", "Amanda", "Amara"];
 
@@ -30,3 +17,16 @@ const addToStart = (string, array) => {
   }
 };
 console.log(addToStart("Thelma", students));
+
+//IF STATEMENT
+let students = ["Ada", "Amaka", "Amanda", "Amara"];
+
+const addToStart = (string, array) => {
+  if (typeof string !== "string") {
+    return "error - can only add strings to an array";
+  } else {
+    array.unshift(string);
+    return array;
+  }
+};
+console.log(addToStart("Tess", students));
