ecfa37c39d130a31bca2436cbab07d742234f9de
Kenneth Onu <kennetho.onu@gmail.com>
2022-07-09 05:38:32 +0800

    Check if an element is in an array

---
+++ elementchecker.js
@@ -0,0 +1,3 @@
+/*
+Create a function named elementChecker that will check a passed in array if at least one of its elements has the same value as a passed in argument. If array is empty, return the message "error - passed in array is empty". Otherwise, return a boolean value depending on the result of the check. Use the students array and the string "Jane" as arguments when testing.
+*/
