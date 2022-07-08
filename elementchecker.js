634719964eff50f3d56b3f70bffcf0f7ccb646e9
Kenneth Onu <kennetho.onu@gmail.com>
2022-07-09 05:45:27 +0800

    Check if an element is in an array

--- elementchecker.js
+++ elementchecker.js
@@ -5,3 +5,11 @@ Create a function named elementChecker that will check a passed in array if at l
 let house = [];
 let students = ["Ada", "Amaka", "Amanda", "Amara"];
 
+const elementChecker = (word, array) => {
+  if (array.length === 0) {
+    return "error - passed in array is empty";
+  } else {
+    return array.some((element) => element === word);
+  }
+};
+
