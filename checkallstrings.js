1a4b19aca13cf63e6d718984db44df474cd15215
Kenneth Onu <kennetho.onu@gmail.com>
2022-07-11 07:07:03 +0800

    Check all ending of strings in an array

---
+++ checkallstrings.js
@@ -0,0 +1,12 @@
+/*
+Create a function named checkAllStringsEnding that will accept a passed in array and a character. The function will do the ff:
+
+if array is empty, return "error - array must NOT be empty"
+if at least one array element is NOT a string, return "error - all array elements must be strings"
+if 2nd argument is NOT of data type string, return "error - 2nd argument must be of data type string"
+if 2nd argument is more than 1 character long, return "error - 2nd argument must be a single character"
+if every element in the array ends in the passed in character, return true. Otherwise return false.
+
+Use the students array and the character "e" as arguments when testing.
+*/
+
