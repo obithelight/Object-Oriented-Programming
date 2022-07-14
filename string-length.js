672cb0990456fc9743a2e1df081cd7d5d4b261f2
Kenneth Onu <kennetho.onu@gmail.com>
2022-07-14 10:33:51 +0800

    Sort array elements according to length of elements

--- string-length.js
+++ string-length.js
@@ -2,3 +2,4 @@
 Create a function named stringLengthSorter that will take in an array of strings as its argument and sort its elements in an ascending order based on their lengths. If at least one element is not a string, return "error - all array elements must be strings". Otherwise, return the sorted array. Use the students array to test.
 */
 
+let students = ["Ifeanyi", "Adanna", "Onyinye", "Chidi", "Ugochi"];
