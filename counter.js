5ff2906b08362e9af0be9269d7833aef0c2e285c
Kenneth Onu <kennetho.onu@gmail.com>
2022-07-16 15:10:12 +0800

    Starts With Counter

---
+++ counter.js
@@ -0,0 +1,12 @@
+/*
+Create a function named startsWithCounter that will take in an array of strings and a single character. The function will do the ff:
+
+if array is empty, return "error - array must NOT be empty"
+if at least one array element is NOT a string, return "error - all array elements must be strings"
+if 2nd argument is NOT of data type string, return "error - 2nd argument must be of data type string"
+if 2nd argument is more than 1 character long, return "error - 2nd argument must be a single character"
+return the number of elements in the array that start with the character argument, must be case-insensitive
+
+Use the students array and the character "J" as arguments when testing.
+*/
+
