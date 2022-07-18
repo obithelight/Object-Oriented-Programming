e0742e8fdad56114933d36f2d38b2f65015bd53e
Kenneth Onu <kennetho.onu@gmail.com>
2022-07-18 17:20:44 +0800

    Randomly pick elements from an array

--- randompicker.js
+++ randompicker.js
@@ -1,2 +1,4 @@
 //Create a function named randomPicker that will take in an array and output any one of its elements at random when invoked. Pass in the students array as an argument when testing.
 
+let students = ["Adamma", "Adaugo", "Chidi", "Nnenna", "Amaka"];
+
